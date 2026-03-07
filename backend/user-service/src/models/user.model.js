a3753def985bb72ad03464e9978aa3a4625902c7
Satish <89683535+softdevsatish@users.noreply.github.com>
2026-03-07 22:43:48 +0530

    hash password

--- backend/user-service/src/models/user.model.js
+++ backend/user-service/src/models/user.model.js
@@ -1,12 +1,30 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
 const userSchema = new mongoose.Schema(
   {
-    name: String,
-    email: { type: String, unique: true },
+    name: { type: String, required: [true, "Name is required"] },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: { type: String, required: true, minlength: 8 },
+    mobileNumber: String,
+    role: String,
   },
   { timestamps: true },
 );
+//Hash Password (pre-save middleware)
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
+//Helper to Compare Passwords
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 const User = mongoose.model("User", userSchema);
 export default User;
